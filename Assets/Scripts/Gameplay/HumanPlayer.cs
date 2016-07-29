82aecb681494ccb7c15d505ff5cee6bca4397215
PhilKlae <pklaeser@hs-mittweida.de>
2016-07-30 00:41:30 +0200

    Added Player Ui and GameController

---
+++ Assets/Scripts/Gameplay/HumanPlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayer : MonoBehaviour {
+
+
+	public string nameHuman;
+	public int kills;
+	public int deaths;
+	public GameObject playerInfoUi;
+	public int playerNumber;
+	 UIUpdater uiUpdater;
+
+	//character char
+	// Use this for initialization
+	void Start () {
+		GameController.totalPlayers = 1;
+		kills = 0;
+		deaths = 0;
+		GameObject g =Instantiate (playerInfoUi);
+		g.transform.parent = GameObject.Find ("Canvas").transform;
+		g.GetComponent<RectTransform> ().position = new Vector3 ((Screen.width / 2) / GameController.totalPlayers * playerNumber,200,0);
+		uiUpdater = g.GetComponent<UIUpdater> ();
+		uiUpdater.player = this;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+	}
+}
---
+++ Assets/Scripts/UI/UIUpdater.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIUpdater : MonoBehaviour {
+	public Image characterImage;
+	public Image healthBar;
+	public Text killCounter;
+	public Text deathCounter;
+	public Text playerName;
+	public bool refresh;
+	public HumanPlayer player;
+	public Material m;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		healthBar.material.EnableKeyword("_Health");
+		m = Instantiate (healthBar.material);
+		healthBar.material = m;
+
+		playerName.text = player.nameHuman;
+		killCounter.text = "" + player.kills;
+		deathCounter.text ="" +  player.deaths;
+	
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(refresh){
+			killCounter.text = "" + player.kills;
+			deathCounter.text ="" +  player.deaths;
+
+		}
+
+	}
+
+
+	public void repaint(){
+		refresh = true;
+	}
+
+
+
+
+}
