3c3edf9906be919cec0ab996d09da0be35d5f3dd
steveson <ssonnta1@hs-mittweida.de>
2016-07-29 23:48:30 +0200

    layer

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
+	public string blockType;
+	public bool isVisible;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	void ChangeVisibility(int visibility){
+		if(visibility == 0){
+			isVisible = false;
+			GetComponent<MeshRenderer>().enabled = false;
+			GetComponent<BoxCollider2D>().enabled = false;
+		}
+		else if(visibility == 1){
+			isVisible = true;
+			GetComponent<MeshRenderer>().enabled = true;
+			GetComponent<BoxCollider2D>().enabled = true;
+		}
 	}
 }
