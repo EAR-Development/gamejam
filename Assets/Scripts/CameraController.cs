90d567c773f90db875615d0a811e0671a1f93166
Alexander Engelsberger <alex@ear-dev.de>
2016-07-30 18:16:07 +0200

    rotate skbox

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,11 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject[] allPlayer;
 
+	private float xMin;
+	private float xMax;
+	private float yMin;
+	private float yMax;
+
 	// Use this for initialization
 	void Start () {
 		allPlayer = GameObject.FindGameObjectsWithTag("Player");
@@ -12,6 +17,25 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		calculateContainingRectangle ();
+		Debug.Log (xMin + ":" + xMax);
+	}
+
+	void calculateContainingRectangle(){
+		xMin = 20;
+		xMax = -10;
+		yMin = 10;
+		yMax = 0;
+		foreach (GameObject player in allPlayer) {
+			float x = player.transform.position.x;
+
+			xMin = Mathf.Min (x, xMin);
+			xMax = Mathf.Max (x, xMax);
+
+			float y = player.transform.position.y;
+
+			yMin = Mathf.Min (y, yMin);
+			yMax = Mathf.Max (y, yMax);
+		}
 	}
 }
