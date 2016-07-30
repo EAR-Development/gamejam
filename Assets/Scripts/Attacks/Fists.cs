b1ce98d2e118fef6befdf979a6c7957b659e0e09
steveson <ssonnta1@hs-mittweida.de>
2016-07-30 19:56:41 +0200

    atk

--- Assets/Scripts/Attacks/Fists.cs
+++ Assets/Scripts/Attacks/Fists.cs
@@ -14,8 +14,9 @@ public class Fists : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter(Collision col ){
-		//if(col.gameObject.tag == "Player"){
+		if(col.gameObject.tag == "Player"){
 			Debug.Log("player hit");
-		//}
+			col.rigidbody.AddForce(transform.up * 600);
+		}
 	}
 }
