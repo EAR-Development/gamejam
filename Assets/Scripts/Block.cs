0eb24cc5528879afd321062eb185f214f186a53d
steveson <ssonnta1@hs-mittweida.de>
2016-07-31 13:45:03 +0200

    bounce

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -12,6 +12,8 @@ public class Block : MonoBehaviour {
 	public Material slowMaterial;
 	public Material fireMaterial;
 	public Material invisMaterial;
+	public Material bounceMaterial;
+	public PhysicMaterial bouncePhysicMaterial;
 
 	public Renderer rend;
 	
@@ -35,8 +37,10 @@ public class Block : MonoBehaviour {
 				ChangeBlockType ("Invis");
 			} else if (rand >= 0.95f) {
 				ChangeBlockType ("Slow");
-			} else if (rand >= 0.9f) {
+			} else if (rand >= 0.93f) {
 				ChangeBlockType ("Fire");
+			} else if (rand >= 0.91f) {
+				ChangeBlockType ("Bounce");
 			}
 		} else {
 			var rand = Random.Range (0f, 1f);
@@ -65,15 +69,23 @@ public class Block : MonoBehaviour {
 		switch(type){
 		case "Slow":
 			rend.sharedMaterial = slowMaterial;
+			transform.GetComponent<BoxCollider>().material = null;
 			break;
 		case "Fire":
 			rend.sharedMaterial = fireMaterial;
+			transform.GetComponent<BoxCollider>().material = null;
 			break;
 		case "Normal":
 			rend.sharedMaterial = normalMaterial;
+			transform.GetComponent<BoxCollider>().material = null;
 			break;
 		case "Invis":
 			rend.sharedMaterial = invisMaterial;
+			transform.GetComponent<BoxCollider>().material = null;
+			break;
+		case "Bounce":
+			rend.sharedMaterial = bounceMaterial;
+			transform.GetComponent<BoxCollider>().material = bouncePhysicMaterial;
 			break;
 		}
 	}
