4911ef66c03b93aa358fa3dd2a3e198eeb5f9b1b
steveson <ssonnta1@hs-mittweida.de>
2016-07-31 16:28:56 +0200

    music

---
+++ Assets/StartMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMusic : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip clip;
+	public float counter = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(counter > 0){
+			counter -= Time.deltaTime;
+		}
+		else if(counter <= 0){
+			if(!source.isPlaying){
+				source.clip = clip;
+				source.loop = true;
+				source.Play();
+			}
+		}
+	}
+}
