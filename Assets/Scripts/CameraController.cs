e86e8c57b3c28904ff7ce3dc7ea844e4adc8a8c6
Alexander Engelsberger <alex@ear-dev.de>
2016-07-31 12:02:44 +0200

    Camera Closer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject[] allPlayer;
 
-	private static float MINIMUMDISTANCE = 18f;
+	private static float MINIMUMDISTANCE = 13f;
 
 	private float xMin;
 	private float xMax;
@@ -37,17 +37,22 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 		calculateContainingRectangle ();
 
-		float d = Mathf.Max (xMax - xMin, yMax - yMin, 20);
+		float d;
 
 		float xDistance = (xMax - xMin) * fovXFactor;
 		float yDistance = (yMax - yMin) * fovYFactor;
 
-		d = Mathf.Max (xDistance, yDistance, MINIMUMDISTANCE);
+
+		if (GameController.center.activeRotation != EnvironmentController.Rotations.NOROTATION) {
+			d = Mathf.Max (xDistance, yDistance, MINIMUMDISTANCE + 5);
+		} else {
+			d = Mathf.Max (xDistance, yDistance, MINIMUMDISTANCE);
+		}
 
 		float speedFactor = 1.8f;
 
 		Vector3 distance = transform.position - new Vector3 ((xMin + xMax) / 2, (yMin + yMax) / 2, -d);
-		if (distance.magnitude > 5) {
+		if (distance.magnitude > 9) {
 			speedFactor = 0.5f;
 		}
 
