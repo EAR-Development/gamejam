4672a0e383ccd1cb4c2b9acb59be901dbe40bcd1
steveson <ssonnta1@hs-mittweida.de>
2016-07-31 03:57:57 +0200

    dd

---
+++ Assets/Scripts/Animator/DestructionDetlef/DD_KickAtk.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DD_KickAtk : StateMachineBehaviour
+{
+	public Rigidbody rb;
+	public Collider[] colliders;
+	public BaseCharacter characterScript;
+	
+    // This will be called when the animator first transitions to this state.
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		rb = animator.GetComponent<Rigidbody>();
+		characterScript = animator.GetComponent<BaseCharacter>();
+		colliders = characterScript.col_fists;
+		
+		colliders[2].enabled = true;
+		//colliders[1].enabled = true;
+    }
+
+
+    // This will be called once the animator has transitioned out of the state.
+    override public void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		colliders[2].enabled = false;
+		//colliders[1].enabled = false;
+    }
+	
+	override public void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		
+		
+    }
+
+
+    // This will be called every frame whilst in the state.
+    override public void OnStateIK (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
