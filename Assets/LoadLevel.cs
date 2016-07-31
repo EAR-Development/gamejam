f0f4250b853966b0b0f743d31bf18aad8f4424f8
steveson <ssonnta1@hs-mittweida.de>
2016-07-31 19:40:11 +0200

    new

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void LoadMenu(){
+		
+		Application.LoadLevel(0);
+
+	}
+
+	
+}
